22eb8e5fcc46aa12f95fd611d91e8c4836d15f13
Mate Radnai <mate_radnai@epam.com>
2022-09-01 16:57:48 +0200

    update files

--- ajv-instance.js
+++
@@ -1,8 +0,0 @@
-const Ajv = require("ajv");
-const addFormats = require("ajv-formats");
-const config = require("./json-schema.config.json");
-
-const ajvInstance = new Ajv(config);
-addFormats(ajvInstance);
-
-module.exports = ajvInstance;
